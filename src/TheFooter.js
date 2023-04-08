9d2b379eddeefa71a0dc50d81b3065b028659c00
manozr <manozregmee@gmail.com>
2023-04-08 16:04:44 +0300

    Example commitII

---
+++ src/TheFooter.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./TheFooter.css";
+function Footer(){
+   return(
+   <h1 class="footer-component">Footer</h1>
+   )
+}
+
+export default Footer;
---
+++ src/TheHeader.js
@@ -0,0 +1,9 @@
+ import React from "react";
+ import "./TheHeader.css";
+ function Header(){
+    return(
+    <h1 class="header-component">I am a Header</h1>
+    )
+ }
+
+ export default Header;
